e5073497d04da211ae51965ee314cc5213af3791
Lukasz Szwajkowski <szwajkowskilukasz@gmail.com>
2015-06-17 23:14:57 +0200

    gradle wrapper + async controller

--- src/main/java/training/AsynchronousExampleApplication.java
+++ src/main/java/training/AsynchronousExampleApplication.java
@@ -2,8 +2,10 @@ package training;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("training")
 public class AsynchronousExampleApplication {
 
     public static void main(String[] args) {
