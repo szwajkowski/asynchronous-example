531156b921d537011b1b095a042f7ccb4a18388c
Lukasz Szwajkowski <szwajkowskilukasz@gmail.com>
2015-06-17 20:36:26 +0200

    project structure

---
+++ src/main/java/training/AsynchronousExampleApplication.java
@@ -0,0 +1,12 @@
+package training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsynchronousExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsynchronousExampleApplication.class, args);
+    }
+}
